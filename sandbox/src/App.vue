9e2a716bd5b2acee701377ad920bda9a829f807a
José Alves <jose.alves@25friday.com>
2023-02-16 11:05:15 +0000

    created vue project with vite

---
+++ sandbox/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello it works!",
+      listOfNumbers: [1, 2, 3, 4, 5, 6],
+    };
+  },
+};
+</script>
+
+<template>
+  <p v-if="message.length % 2 === 0">Even: {{ message }}</p>
+  <p v-else>Odd: {{ message }}</p>
+  <ul>
+    <li v-for="(n, index) in listOfNumbers" :key="`item-${index}`">{{ n }}</li>
+  </ul>
+</template>
